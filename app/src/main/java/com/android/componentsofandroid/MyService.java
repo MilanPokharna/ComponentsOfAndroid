760298877d21eb9aff3046f816c58968d48a5633
MiLaN <milan.pokharna1998@gmail.com>
2018-09-08 11:37:01 +0530

    initial commit

--- app/src/main/java/com/android/componentsofandroid/MyService.java
+++ app/src/main/java/com/android/componentsofandroid/MyService.java
@@ -3,14 +3,26 @@ package com.android.componentsofandroid;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class MyService extends Service {
     public MyService() {
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
