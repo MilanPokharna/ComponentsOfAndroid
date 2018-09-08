4a34ec3c015ce3aedd50782edf3c5f868635a8af
MiLaN <milan.pokharna1998@gmail.com>
2018-09-08 11:40:29 +0530

    initial commit

--- app/src/main/java/com/android/componentsofandroid/MyService.java
+++ app/src/main/java/com/android/componentsofandroid/MyService.java
@@ -10,6 +10,12 @@ public class MyService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Service Created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
         return super.onStartCommand(intent, flags, startId);
